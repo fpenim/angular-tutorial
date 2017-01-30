f9423fe6a226fd08ecf0f2b7c63d6cb0d5517506
Flavia Penim <flaviapenim@gmail.com>
2017-01-30 15:43:22 +0000

    Create new hero

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -38,6 +38,14 @@ export class HeroService {
       .catch(this.handleError);
   }
 
+  create(name: string): Promise<Hero> {
+    return this.http
+      .post(this.heroesUrl, JSON.stringify({name: name}), {headers: this.headers})
+      .toPromise()
+      .then(response => response.json().data)
+      .catch(this.handleError);
+  }
+
   private handleError(error: any): Promise<any> {
     console.error('An error has occurred', error); //demo purposes only
     return Promise.reject(error.message || error);
